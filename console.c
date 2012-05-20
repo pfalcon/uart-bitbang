d4087746dca0eceffe164b2d1c520356756cf880
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-05-20 19:54:09 +0300

    Simple console helper functions.

---
+++ console.c
@@ -0,0 +1,34 @@
+#include "uart_bitbang.h"
+
+void newline()
+{
+    uartbb_tx('\r');
+    uartbb_tx('\n');
+}
+
+void puthex4(uint8_t b)
+{
+    b += '0';
+    if (b > '9')
+        b += 7;
+    uartbb_tx(b);
+}
+
+void puthex8(uint8_t b)
+{
+    puthex4(b >> 4);
+    puthex4(b & 0xf);
+}
+
+void puthex16(uint16_t v)
+{
+    puthex8(v >> 8);
+    puthex8(v  & 0xff);
+}
+
+void putstr(char *s)
+{
+    while (*s) {
+        uartbb_tx(*s++);
+    }
+}
---
+++ console.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+void puthex8(uint8_t val);
+void puthex16(uint16_t val);
+void putstr(char *s);
+void newline();
