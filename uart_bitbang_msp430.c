4380753b7ccfdd628e71a3d5608924e964ac85f5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-05-20 20:47:30 +0300

    Allow RXD/TXD pin assignments be (compile-time) configurable.
    If USE_COMPILE is defined, "uart_bitbang_config.h" will be included,
    which is expected to redefine port and pin assignments.

--- uart_bitbang_msp430.c
+++ uart_bitbang_msp430.c
@@ -1,8 +1,19 @@
 #include <msp430.h>
 #include "uart_bitbang.h"
 
-#define TXD BIT1
-#define RXD BIT2
+// Allow uart_bitbang_config.h to override port and pins to use for UART
+#ifdef USE_CONFIG
+#include "uart_bitbang_config.h"
+#endif
+
+#ifndef UART_BITBANG_PIN_TXD
+#define UART_BITBANG_PORT_RXD_DIR P1DIR
+#define UART_BITBANG_PORT_TXD_DIR P1DIR
+#define UART_BITBANG_PORT_RXD     P1IN
+#define UART_BITBANG_PORT_TXD     P1OUT
+#define UART_BITBANG_PIN_TXD      BIT1
+#define UART_BITBANG_PIN_RXD      BIT2
+#endif
 
 
 uint16_t bit_time;
@@ -10,8 +21,12 @@ uint16_t half_bit_time;
 
 void uartbb_init()
 {
-    P1DIR &= ~RXD;
-    P1DIR |= TXD;
+#if UART_BITBANG_PORT_RXD_DIR == P2DIR || UART_BITBANG_PORT_TXD_DIR == P2DIR
+    // Set (entire) P2 as GPIO
+    P2SEL = 0;
+#endif
+    UART_BITBANG_PORT_RXD_DIR &= ~UART_BITBANG_PIN_RXD;
+    UART_BITBANG_PORT_TXD_DIR |= UART_BITBANG_PIN_TXD;
     // SMCLK, continuous mode
     TACTL = TASSEL_2 + MC_2;
 }
@@ -19,9 +34,9 @@ void uartbb_init()
 void uartbb_autodetect_baud()
 {
     uint16_t start, stop;
-    while (P1IN & RXD);
+    while (UART_BITBANG_PORT_RXD & UART_BITBANG_PIN_RXD);
     start = TAR;
-    while (!(P1IN & RXD));
+    while (!(UART_BITBANG_PORT_RXD & UART_BITBANG_PIN_RXD));
     stop = TAR;
     bit_time = stop - start;
     half_bit_time = bit_time / 2;
@@ -34,20 +49,20 @@ void uartbb_tx(uint8_t val)
 
     start = TAR;
 
-    P1OUT &= ~TXD;
+    UART_BITBANG_PORT_TXD &= ~UART_BITBANG_PIN_TXD;
     while (TAR - start < bit_time);
 
     do {
         if (val & 1)
-            P1OUT |= TXD;
+            UART_BITBANG_PORT_TXD |= UART_BITBANG_PIN_TXD;
         else
-            P1OUT &= ~TXD;
+            UART_BITBANG_PORT_TXD &= ~UART_BITBANG_PIN_TXD;
         start += bit_time;
         while (TAR - start < bit_time);
         val >>= 1;
     } while (--bit_count);
 
-    P1OUT |= TXD;
+    UART_BITBANG_PORT_TXD |= UART_BITBANG_PIN_TXD;
     start += bit_time;
     while (TAR - start < bit_time);
 }
@@ -58,15 +73,15 @@ uint8_t uartbb_rx()
     uint8_t val;
     uint8_t b = 0, bit_count = 8;
 
-    while (P1IN & RXD);
+    while (UART_BITBANG_PORT_RXD & UART_BITBANG_PIN_RXD);
     start = TAR;
-    do val = P1IN; while (TAR - start < half_bit_time);
+    do val = UART_BITBANG_PORT_RXD; while (TAR - start < half_bit_time);
     start += half_bit_time;
 
     do {
-        do val = P1IN; while (TAR - start < bit_time);
+        do val = UART_BITBANG_PORT_RXD; while (TAR - start < bit_time);
         b >>= 1;
-        if (val & RXD)
+        if (val & UART_BITBANG_PIN_RXD)
             b |= 0x80;
         start += bit_time;
     } while (--bit_count);
